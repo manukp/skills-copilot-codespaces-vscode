fec331eceada52a638372b90092b99e4cb1e5cb4
manukp <manukp.leo@gmail.com>
2023-07-18 19:13:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var memberContent = document.getElementById("memberContent");
+    var skillsContent = document.getElementById("skillsContent");
+    var projectsContent = document.getElementById("projectsContent");
+    var contactContent = document.getElementById("contactContent");
+    var aboutContent = document.getElementById("aboutContent");
+    member.style.backgroundColor = "#f2f2f2";
+    skills.style.backgroundColor = "#fff";
+    projects.style.backgroundColor = "#fff";
+    contact.style.backgroundColor = "#fff";
+    about.style.backgroundColor = "#fff";
+    memberContent.style.display = "block";
+    skillsContent.style.display = "none";
+    projectsContent.style.display = "none";
+    contactContent.style.display = "none";
+    aboutContent.style.display = "none";
+}
